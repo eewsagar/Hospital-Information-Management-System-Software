21e940f956794af39dff5b8d02e2873e10515eb9
18sarthak18 <151671830+18sarthak18@users.noreply.github.com>
2024-03-05 17:54:21 +0530

    added a folder structuer and script file

---
+++ HIMS/src/main/java/com/main/dto/employee.java
@@ -0,0 +1,15 @@
+package com.main.dto;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sarthak
+ */
+public class employee {
+    
+}
